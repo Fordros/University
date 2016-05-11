8f2133f7524a1ca77fa824b4450092c1ce985ed8
Rostyslav Ivaschenko <fordrosw@gmail.com>
2016-05-11 23:16:21 +0300

    Добавлен JSTL

--- src/main/java/com/university/servlet/StudentServlet.java
+++ src/main/java/com/university/servlet/StudentServlet.java
@@ -12,27 +12,27 @@ import javax.servlet.http.HttpServletResponse;
 import com.university.dao.DaoFactory;
 import com.university.dao.impl.DaoFactoryImpl;
 import com.university.domain.Student;
+import com.university.exception.DaoException;
 
 /**
  * Servlet implementation class Student
  */
-@WebServlet("/Student")
-public class Student extends HttpServlet {
+@WebServlet("/student")
+public class StudentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	DaoFactory factory = new DaoFactoryImpl();
-    List<Student> students =  factory.getDao(Student.class).getAll();
+
+    	DaoFactory factory = new DaoFactoryImpl();
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			List<Student> students =  factory.getDao(Student.class).getAll();
+		} catch (DaoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		request.getRequestDispatcher("student.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
 
 }
